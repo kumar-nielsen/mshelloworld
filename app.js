b79633787a1c7be865151fdc059d2e5680a45a37
Sarthak Kumar <sarthakk92@gmail.com>
2016-09-07 16:10:59 -0500

    Added endpoint for bot

--- app.js
+++ app.js
@@ -11,12 +11,18 @@ server.listen(process.env.PORT || 3000, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
 
+// Query parser
+server.use(restify.queryParser());
+
 // Create chat bot
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
+
+// Bot endpoint
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
 
 // USED FOR CONSOLE CONNECTOR
 // var connector = new builder.ConsoleConnector().listen();
